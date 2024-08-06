44d8d915b6925c4974293a1732b3ba5e6c6a4cdb
Sachin Chanduveetil <sachisavera@gmail.com>
2024-08-06 15:05:19 +0530

    Create RemoveDupliArray.ts

---
+++ RemoveDupliArray.ts
@@ -0,0 +1,10 @@
+const a= [1,1,2,3,4,5,5];
+// Function to remove duplicates from an array
+function removeDuplicates(arr: any[]): any[] {
+    return [...new Set(arr)];
+
+  
+}
+// Test Output
+const uniqueArray = removeDuplicates(testArray);
+console.log(`Array without duplicates: ${uniqueArray}`);
